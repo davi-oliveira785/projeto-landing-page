cb899a3b49bc035ab2b5c30dee863843c40d7699
davi-oliveira785 <davi.oliveira30@gmail.com>
2023-10-30 22:01:37 -0300

    versao inicial

---
+++ Projeto/index.js
@@ -0,0 +1,19 @@
+var setaDireita = window.document.getElementById("setaDireita")
+var bruna = window.document.getElementById("bruna")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var setaEsquerda = window.document.getElementById("setaEsquerda")
+
+function RolarParaDireita(){
+    bruna.style  = "display:none"
+    samantha.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display: flex"
+}
+
+function RolarParaEsquerda(){
+    samantha.style = "display:none"
+    bruna.style = "display:flex"
+    setaEsquerda.style = "display:none"
+    setaDireita.style = "display:flex"
+}
